c6c3e887f8ca030858105867c8132bb7d95f42bc
Olowu Juwon <juwonolowu52@gmail.com>
2021-03-10 21:23:15 +0100

    Calculator project

---
+++ js/calculator.js
@@ -0,0 +1,7 @@
+// Your task is to add interactivity to the Calculator UI given. 
+// User Story
+// The user will enter numeric values in the input elements of type number.
+// To have the result, the user would click on any of the five buttons *, -, +, /, %
+// Based on the button clicked, your app should perform the expected calculation and display the answer in the input element of type text.
+// Best of luck!
+
